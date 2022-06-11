c871bd6578439a9682660a4ac1631c2c4c5e6c12
Alex Bertran <70992408+ApBertran@users.noreply.github.com>
2022-06-11 15:36:18 -0700

    Create biomass.py

---
+++ biomass.py
@@ -0,0 +1,10 @@
+num = float(input("Number of fish (N): "))
+a = float(input("A Value: "))
+length = float(input("Average length(cm): "))
+b = float(input("B Value: "))
+
+biomass = "{:.2f}".format(num * a * length ** b)
+
+print("Biomass(kg): ", biomass)
+while True:
+  pass
