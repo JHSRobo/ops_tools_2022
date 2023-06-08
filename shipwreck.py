322adcb32ca72e440ceb52cdd598350b505a2eb1
James Randall <84990498+James-Randall-14@users.noreply.github.com>
2023-06-07 22:44:44 -0700

    Update shipwreck.py
    left a comment documenting a known error

--- shipwreck.py
+++ shipwreck.py
@@ -28,7 +28,7 @@ while True:
     for click in clicks:
        img = cv2.circle(img,(click[0],click[1]),10,(255,0,0),-1)
     if len(clicks) == 4:
-        reference = abs(clicks[0][0] - clicks[1][0])
+        reference = abs(clicks[0][0] - clicks[1][0]) # fkn brainlet error, this only takes the x distance between the two clicks. If ur reusing this code, use pyth. thm to find distance. - James 2023
         ratio = 40. / reference
         total = ((((abs(clicks[2][0] - clicks[3][0]) ** 2) + (abs(clicks[2][1] - clicks[3][1])) ** 2)) ** 0.5) * ratio
         displayImg = np.zeros((512,1024,3), dtype=np.uint8)
